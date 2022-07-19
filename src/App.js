42ffb51d177b28b21380f160b3fd2f3b91bccae3
2Dainius <dainius.drgs@gmail.com>
2022-07-19 19:19:52 +0300

    Styling sidebar starting main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import StackOverflow from './components/StackOverflow';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route  exact path='/' component = {StackOverflow} />
+        </Switch>
+      </Router>
     </div>
   );
 }
