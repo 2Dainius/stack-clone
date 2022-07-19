cc580f7a5d2544c246127ec522d32fe6edefc92a
2Dainius <dainius.drgs@gmail.com>
2022-07-19 21:07:05 +0300

    Styling main and question page

---
+++ src/components/StackOverflow/AllQuestions.js
@@ -0,0 +1,62 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './css/AllQuestions.css';
+
+function AllQuestions() {
+    return (
+        <div className="all-questions">
+          <div className="all-questions-container">
+            <div className="all-questions-left">
+              <div className="all-options">
+                <div className="all-option">
+                  <p>0</p>
+                  <span>votes</span>
+                </div>
+                <div className="all-option">
+                  <p>0</p>
+                  <span>answers</span>
+                </div>
+                <div className="all-option">
+                  <small>0 views</small>
+                </div>
+              </div>
+            </div>
+            <div className="question-answer">
+              <Link>This is question title or smth</Link>
+              <div
+                style={{
+                  maxWidth: "90%",
+                }}
+              >
+                <div>AI Dungeon is a free-to-play single-player and multiplayer text adventure game which uses artificial intelligence to generate content. It allows players to create and share their own custom adventure settings.</div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <span className='question-tags'>
+                    React
+                </span>
+                <span className='question-tags'>
+                    AI
+                </span>
+                <span className='question-tags'>
+                    Random
+                </span>
+              </div>
+              <div className="author">
+                <small>Timelapse</small>
+                <div className="auth-details">
+                  <Avatar />
+                  <p>User Name</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+export default AllQuestions
--- src/components/StackOverflow/Main.js
+++ src/components/StackOverflow/Main.js
@@ -1,10 +1,49 @@
+import { FilterList } from '@mui/icons-material'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AllQuestions from './AllQuestions'
+import './css/Main.css';
 
 function Main() {
   return (
     <div className='main'>
-
+        <div className='main-conteiner'>
+            <div className='main-top'>
+                <h2>All Questions</h2>
+                <Link>
+                    <button>Ask Question</button>
+                </Link>
+            </div>
+            <div className='main-description'>
+                <p>All questions stat</p>
+                <div className='main-filter'>
+                    <div className='main-tabs'>
+                        <div className='main-tab'>
+                            <Link>Newest</Link>
+                        </div>
+                        <div className='main-tab'>
+                            <Link>Active</Link>
+                        </div>
+                        <div className='main-tab'>
+                            <Link>More</Link>
+                        </div>
+                    </div>
+                    <div className='main-filter-item'>
+                        <FilterList />
+                        <p>Filter</p>
+                    </div>
+                </div>
+            </div>
+            <div className='questions'>
+                <div className='question'>
+                    <AllQuestions />
+                    <AllQuestions />
+                    <AllQuestions />
+                    <AllQuestions />
+                    <AllQuestions />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
